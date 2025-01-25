ebf0487332ef6a1188fb9b1b5b288d80835d5826
David <davidifme@icloud.com>
2025-01-25 14:32:53 +0100

    Enable toDoManager usage in Chrome DevTools
    Previously, the toDoManager functionality was not accessible in
    Chrome DevTools, limiting its debugging and testing capabilities.
    
    Added the ability to use toDoManager directly in DevTools for
    improved developer experience. Additionally, new utility functions
    were implemented to enhance task and project management,
    including addTask(), removeTask(), moveTask(), getTaskByID(),
    getProjectByID(), getCurrentProject(), and setCurrentProject().

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import "/styles/styles.css";
-import "/styles/main-content.css";
-import "/styles/sidebar.css"
-import "/styles/reset.css";
-import "/styles/new-todo-modal.css";
-import "/styles/font-family-combo.css";
-import { toDoManager } from "./toDoManager.js";
-import { toDoUI } from "./toDoUI.js";
+import { toDoManager } from "./toDoManager";
 
-// Console usage in Chrome DevTool
 window.toDoManager = toDoManager;
-window.toDoUI = toDoUI;
 
-toDoManager.createProject('test');
-toDoManager.createProject('tes2t');
-toDoManager.createProject('test3');
+toDoManager.addTask('Test task')
+toDoManager.addTask('Task')
+toDoManager.addTask('Mama')
 
-toDoManager.addToDo();
-toDoManager.addToDo();
-toDoManager.addToDo();
-toDoManager.addToDo();
-
-toDoUI.renderContent();
+toDoManager.createProject('New Project')
--- src/toDoManager.js
+++ src/toDoManager.js
@@ -1,296 +1,116 @@
-import { idGenerator } from "./idGenerator.js";
+import { idGenerator } from "./idGenerator";
 
 export const toDoManager = (function() {
-    let toDoProjects = [];
-    const defaultProject = createProject('Default');
-    let currentProject = defaultProject;
 
-    function getProjects() {
-        return toDoProjects;
+    const projects = [];
+    let defaultProject = createProject('Default')
+    let currentProject = defaultProject;
+    
+    function addTask(title, description, dueDate, priority, projectID) {
+        const newTask = createToDo(title, description, dueDate, priority);
+
+        if (!projectID) {
+            getCurrentProject().tasks.push(newTask);
+            console.log(`"${newTask.title}" has been added to the project "${getCurrentProject().title}"`);
+        } else {
+            projects.forEach(project => {
+                if (project.id === projectID) {
+                    project.tasks.push(newTask);
+                    console.log(`"${newTask.title}" has been added to the project "${project.title}"`);
+                }
+            });
+        }
     }
 
-    function setCurrentProject(project) {
-        currentProject = project;
+    function removeTask(id) {
+        projects.forEach(project => project.tasks.forEach((task, taskIndex) => {
+            if (task.id === id) {
+                let removedTaskTitle = task.title;
+                project.tasks = project.tasks.filter((_, index) => index !== taskIndex);
+                console.log(`"${removedTaskTitle}" has been removed from "${project.title}"`);
+            }
+        }));
+    }
+
+    function moveTask(taskID, projectID) {
+        const task = getTaskByID(taskID);
+
+        removeTask(taskID);
+        getProjectByID(projectID).tasks.push(task);
+        console.log(`"${task.title}" has been moved to "${getProjectByID(projectID).title}"`);
+    }
+
+    function getTaskByID(id) {
+        for (let project of projects) {
+            for (let task of project.tasks) {
+                if (task.id === id) {
+                    return task;
+                }
+            }
+        }
+        return undefined;
+    }
+
+    function getProjectByID(id) {
+        for (let project of projects) {
+            if (project.id === id) {
+                return project;
+            }
+        }
+        return undefined;
+    }
+
+    function createToDo(title = 'New task', description, dueDate, priority, id = idGenerator.generateUniqueId()) {
+        return {
+            title,
+            description,
+            dueDate,
+            priority,
+            id
+        };
+    }
+
+    function createProject(title = 'New Project', description = '') {
+        let newProject = {
+            title,
+            description,
+            tasks: [],
+            id: idGenerator.generateUniqueId()
+        };
+
+        projects.push(newProject);
+        console.log(`Project: "${newProject.title}" has been created`);
+
+        return newProject;
     }
 
     function getCurrentProject() {
         return currentProject;
     }
 
-    function createToDo
-        (title = 'New Task',
-        description = '',
-        project = 'default',
-        priority = 'none',
-        dueDate,
-        notes = '',
-        completed = false)
-        {
-
-        const id = idGenerator.generateUniqueId();
-        
-        const toDo = {
-            title,
-            description,
-            priority,
-            dueDate,
-            notes,
-            completed,
-            project,
-            id
-        };
-
-        return toDo;
+    function setCurrentProject(project) {
+        currentProject = project;
     }
 
-    function createProject(title = 'New Project', description = '') {
-        const toDos = [];
-        const id = idGenerator.generateUniqueId();
-
-        const newProject = {
-            title,
-            description,
-            toDos,
-            id
-        }
-
-        toDoProjects.push(newProject);
-
-        return newProject;
-    }
-
-    function addToDo
-        (title, 
-        description, 
-        projectTitle = 'default',
-        priority = 'none',
-        dueDate,
-        notes = '',
-        completed = false)
-        {
-        const newToDo = createToDo(title, description, projectTitle, priority, dueDate, notes, completed);
-        const userProject = toDoProjects.find(project => project.title.toLowerCase() === projectTitle.toLowerCase());
-
-        if (newToDo.project === 'default') {
-            defaultProject.toDos.push(newToDo);
-            console.log(`Task with ID:${newToDo.id} has been added to the project "${projectTitle}".`);
-            return;
-        }
-        if (userProject) {
-            userProject.toDos.push(newToDo);
-            console.log(`Task with ID:${newToDo.id} has been added to the project "${projectTitle}".`);
-        } else {
-            console.error(`Project ${projectTitle} doesn't exist.`);
-        }
-    }
-
-    function removeTodo(id) {
-        let isRemoved = false;
-
-        toDoProjects.forEach(project => {
-            const index = project.toDos.findIndex(toDo => toDo.id === id);
-
-            if (index !== -1) {
-                project.toDos.splice(index, 1);
-                console.log(`Task with ID:${id} has been removed from the project "${project.title}".`);
-                isRemoved = true;
-            }
-        });
-
-        if (!isRemoved) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-    
-    function setPriority(id, level = 0) {
-        let isChanged = false;
-
-        switch(level) {
-            case 0:
-                level = 'none';
-                break;
-            case 1:
-                level = 'low';
-                break;
-            case 2:
-                level = 'medium';
-                break;
-            case 3:
-                level = 'high';
-                break;
-            default:
-                console.error('Unknown level of priority.');
-        }
-
-        toDoProjects.forEach(project => {
-            const index = project.toDos.findIndex(toDo => toDo.id === id);
-            if (index !== -1) {
-                project.toDos[index].priority = level;
-                console.log(`Task's priority with ID:${id} has been changed to "${level}".`);
-                isChanged = true;
-            }
-        });
-
-        if (!isChanged) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function setNoteText(id, text = '') {
-        let isChanged = false;
-
-        toDoProjects.forEach(project => {
-            const index = project.toDos.findIndex(toDo => toDo.id === id);
-            if (index !== -1) {
-                project.toDos[index].notes = text;
-                console.log(`Task's notes with ID:${id} has been changed.`);
-                isChanged = true;
-            }
-        });
-
-        if (!isChanged) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function toggleComplete(id) {
-        let isChanged = false;
-
-        toDoProjects.forEach(project => {
-            const index = project.toDos.findIndex(toDo => toDo.id === id);
-            if (index !== -1) {
-                project.toDos[index].completed = !project.toDos[index].completed;
-                console.log(`Task with ID:${id} status has been changed.`);
-                isChanged = true;
-            }
-        });
-
-        if (!isChanged) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function setTitle(id, newTitle = '', type = 'toDo') {
-        let isChanged = false;
-
-        if (type === 'toDo') {
-            toDoProjects.forEach(project => {
-                const index = project.toDos.findIndex(toDo => toDo.id === id);
-                if (index !== -1) {
-                    project.toDos[index].title = newTitle;
-                    console.log(`Task's title with ID:${id} has been changed to "${newTitle}".`);
-                    isChanged = true;
-                }
-            });
-        } else if (type === 'project') {
-            const index = toDoProjects.findIndex(project => project.id === id);
-            if (index !== -1) {
-                toDoProjects[index].title = newTitle;
-                console.log(`Project's title with ID:${id} has been changed to "${newTitle}".`);
-                isChanged = true;
-            }
-        }
-
-        if (!isChanged) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function setDescription(id, newDescription, type = 'toDo') {
-        let isChanged = false;
-    
-        if (type === 'toDo') {
-            toDoProjects.forEach(project => {
-                const index = project.toDos.findIndex(toDo => toDo.id === id);
-                if (index !== -1) {
-                    project.toDos[index].description = newDescription;
-                    console.log(`Task's description with ID:${id} has been changed to "${newDescription}".`);
-                    isChanged = true;
-                }
-            });
-        } else if (type === 'project') {
-            const index = toDoProjects.findIndex(project => project.id === id);
-            if (index !== -1) {
-                toDoProjects[index].description = newDescription;
-                console.log(`Project's description with ID:${id} has been changed to "${newDescription}".`);
-                isChanged = true;
-            }
-        }
-    
-        if (!isChanged) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function moveToDo(id, projectTitle) {
-        let isMoved = false;
-
-        toDoProjects.forEach(project => {
-            const index = project.toDos.findIndex(toDo => toDo.id === id);
-    
-            if (index !== -1) {
-                addToDo(project.toDos[index].title, 
-                    project.toDos[index].description, 
-                    projectTitle, 
-                    project.toDos[index].priority, 
-                    project.toDos[index].dueDate, 
-                    project.toDos[index].notes, 
-                    project.toDos[index].completed);
-                removeTodo(id);
-                console.log(`Task with ID:${id} has been moved to "${projectTitle}".`);
-                isMoved = true;
-            }
-        });
-
-        if (!isMoved) {
-            console.error(`Task with ID:${id} was not found.`);
-        }
-    }
-
-    function cleanProject(id) {
-        let isCleaned = false;
-    
-        const project = toDoProjects.find(project => project.id === id);
-        const index = toDoProjects.findIndex(project => project.id === id);
-    
-        if (project && index !== -1) {
-            project.toDos = [];
-            console.log(`All tasks have been removed from project: "${project.title}".`);
-            isCleaned = true;
-        }
-    
-        if (!isCleaned) {
-            console.error(`Project with ID:${id} was not found.`);
-        }
-    }
-
-    function printToDos() {
-        toDoProjects.forEach(project => {
-            if (project.toDos.length > 0) {
-                console.log(project.toDos);
-            }
+    function printTasks() {
+        projects.forEach(project => {
+            console.log(`Project title: "${project.title}" | ID: ${project.id}`);
+            project.tasks.forEach(task => console.log(`Task title: "${task.title}" | ID: ${task.id}`));
         });
     }
 
     function printProjects() {
-        toDoProjects.forEach(project => console.log(project));
+        projects.forEach(project => console.log(project));
     }
 
     return {
-        createProject,
-        addToDo,
-        removeTodo,
-        setTitle,
-        setDescription,
-        setPriority,
-        setNoteText,
-        moveToDo,
-        toggleComplete,
-        getProjects,
-        cleanProject,
-        setCurrentProject,
         getCurrentProject,
-        printProjects,
-        printToDos
+        setCurrentProject,
+        addTask,
+        removeTask,
+        moveTask,
+        createProject,
+        printTasks,
+        printProjects
     };
 })();
